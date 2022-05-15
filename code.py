ce7d35ddde7e5a32f0739556b48b1dba5280bdf5
Prajwal <prajwalslocker15@gmail.com>
2022-05-15 11:25:39 +0530

    Initial Push

---
+++ code.py
@@ -0,0 +1,9 @@
+import cv2
+import os
+import time
+import uuid
+
+IMAGES_PATH = '/home/shen42/Documents/Final Year project/RealTimeObjectDetection/Tensorflow/workspace/images/collectedimages'
+
+labels = ['hello', 'thanks', 'yes', 'no', 'iloveyou']
+number_imgs = 15
