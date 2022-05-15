bf16f408391ad894177241f2dc569d50768b630f
Prajwal <prajwalslocker15@gmail.com>
2022-05-15 11:48:44 +0530

    Added code for image capturing. Make necessary changes to path and run the code after installing any required dependencies

--- code.py
+++ code.py
@@ -3,7 +3,24 @@ import os
 import time
 import uuid
 
-IMAGES_PATH = '/home/shen42/Documents/Final Year project/RealTimeObjectDetection/Tensorflow/workspace/images/collectedimages'
+IMAGES_PATH = '/home/shen42/Documents/Final Year project/Sign-Language-Detection-B7/Tensorflow/workspace/images/collectedimages'
 
 labels = ['hello', 'thanks', 'yes', 'no', 'iloveyou']
 number_imgs = 15
+
+for label in labels:
+    path = '/home/shen42/Documents/Final Year project/Sign-Language-Detection-B7/Tensorflow/workspace/images/collectedimages/' + label
+    os.mkdir(path)
+    cap = cv2.VideoCapture(0)
+    print('Collecting Images for {}'.format(label))
+    time.sleep(5)
+    for imgnum in range(number_imgs):
+        ret, frame = cap.read()
+        imagename = os.path.join(IMAGES_PATH,label,label+'.'+'{}.jpg'.format(str(uuid.uuid1())))
+        cv2.imwrite(imagename,frame)
+        cv2.imshow('frame',frame)
+        time.sleep(2)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
